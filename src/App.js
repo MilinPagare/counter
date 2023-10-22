1cf121723cd9dbf41ffede046d87225d2652aaea
MilinPagare <pagare.milin1996@gmail.com>
2023-10-22 20:37:31 +0530

    initial update

--- src/App.js
+++ src/App.js
@@ -3,54 +3,50 @@ import "./App.css";
 import Home from "./components/Home";
 
 function App() {
-  const [time, setTime] = useState(0);
-  const [running, setRunning] = useState(false);
+  const [number, setNumber] = useState(0);
+  const [value, setValue] = useState(1);
 
-  useEffect(() => {
-    let intervalId;
-    if (running) {
-      intervalId = setInterval(() => {
-        setTime((time) => time + 1);
-      }, 10);
-    }
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, [running]);
-
-  const miliSec = time % 100;
-  const seconds = Math.floor((time % 6000) / 100);
-  const minutes = Math.floor((time % 360000) / 6000);
-  const hours = Math.floor(time / 360000);
+  const increase = () => {
+    setNumber(number + Number(value));
+  };
+  const decrease = () => {
+    setNumber(number - Number(value));
+  };
 
   return (
     <div className="App">
       <div>
-        <div>
-          <h1>Timer</h1>
-        </div>
-        <div>
-          {hours < 10 ? "0" + hours : hours} :{" "}
-          {minutes < 10 ? "0" + minutes : minutes} :{" "}
-          {seconds < 10 ? "0" + seconds : seconds} :{" "}
-          {miliSec < 10 ? "0" + miliSec : miliSec}
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              setRunning(true);
-            }}
-          >
-            Start
-          </button>
-          <button
-            onClick={() => {
-              setRunning(false);
-            }}
-          >
-            Pause
-          </button>
-        </div>
+        <h1>Counter</h1>
+        <p style={{ fontSize: "3rem", fontWeight: "700" }}>{number}</p>
+        <button
+          style={{
+            border: "1px solid black",
+            height: "2rem",
+            width: "3rem",
+            backgroundColor: "#87CEFA",
+          }}
+          onClick={decrease}
+        >
+          -
+        </button>
+        <button
+          style={{
+            border: "1px solid black",
+            height: "2rem",
+            width: "3rem",
+            backgroundColor: "#87CEFA",
+          }}
+          onClick={increase}
+        >
+          +
+        </button>
+        <br />
+        <label>increase or decrease by : </label>
+        <input
+          type="number"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
       </div>
       {/* <Home/> */}
     </div>
